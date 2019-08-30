dd40fdd4fd20157d20fb7f023dd5cf3f3ad2aaae
unknown <antonoiumadalin@gmail.com>
2019-08-30 14:00:35 +0300

    finished at home

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ var total = 0;
 //1- listen for enter event
 document.getElementById('clientframe').contentWindow.addEventListener('dragenter', function (event) //this listens on dragover for entire iFrame!!
     {
-        event.preventDefault();
         event.stopPropagation();
         console.log('Drag Enter');
-        total += 1;
+
+        //ia elementul tras
+        currentElement = event.target;
+        currentElementChangeFlag = true; //Change Flag to process DragOver Queue
+        //ia detalii despre element
+        elementRectangle = event.target.getBoundingClientRect();
+        countdown = 1;
+        console.log(elementRectangle); //punele pe consola.log
+
+
+
     }, false);
+
 
 //2 - listen for dragover event [If this is not present, you get that cut circle pointer]
 document.getElementById('clientframe').contentWindow.addEventListener("dragover", function (event) //this listens on dragover for entire iFrame!!
@@ -37,6 +47,26 @@ document.getElementById('clientframe').contentWindow.addEventListener("dragover"
         console.log('Drag Over'); //log it
         //There is a known bug, if you dragover the scrollbar, it will start creating lines like crazy 
         //(FIXED NOW by removing .body from line 37 (xox))
+
+
+        /*
+
+        //get mouse coordintes based on window client
+        var x = event.clientX;
+        var y = event.clientY;
+
+        countdown = countdown + 1;
+        currentElementChangeFlag = false;
+
+        var mousePosition = {
+            x: x,
+            y: y
+        };
+
+        console.log(mousePosition);
+        */
+
+
     }, false);
 
 //3 - listen for drop event
@@ -54,16 +84,11 @@ document.getElementById('clientframe').contentWindow.addEventListener("drop", fu
         // Clear the drag data cache (for all formats/types)
         event.dataTransfer.clearData();
 
+
+
         //Need to add it after element
-        
-        //Let`s handle the carry html and 
-        /*
-        //
-        var data = event.dataTransfer.getData("text");
-        // var insertionPoint = $("#clientframe").contents().find(".drop-marker");
-        event.target.appendChild(clientFrameWindow.document.getElementById(data));
-        event.dataTransfer.clearData(); */
-        //not checking for my line here- i should drop where line shows up
+
+
     }, false);
 
 
@@ -82,92 +107,9 @@ function dragstart_handler(ev) {
     console.log("dragStart - Here i should carry data");
 
     ev.dataTransfer.setData("text/plain", ev.target.id);
+    //ev.dataTransfer.effectAllowed = "copy";
+
+
 }
 
-
-//Let`s add a line to show where drop
-
-/* Convert this 
-
-
-
-
-
 //up until here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$(function() { //document ready event  
-    var clientFrameWindow = $('#clientframe').get(0).contentWindow; 
-
-    $("#dragitemslistcontainer li").on('dragstart',function() {
-        console.log("Drag Started");
-    });
-
-    $("#dragitemslistcontainer li").on('dragend',function() {
-        console.log("Drag End");
-    });
-
-    $('#clientframe').on("load", function() // jQuery AJAX here basically saying when frame is loaded, GG, execute
-    {
-        var total = 0; //counter
-
-        $(clientFrameWindow.document.body).find('*').on('dragenter',function(event) //on dragenter every single body element, console log it
-        {
-            event.preventDefault();
-            event.stopPropagation();
-            console.log('Drag Enter');
-            total +=1;
-        }).on('dragover',function(event)
-        {
-            event.preventDefault();
-            event.stopPropagation();
-            console.log('Drag Over');
-            total +=1;
-        });
-        $(clientFrameWindow.document).find('body,html').on('drop',function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            console.log('Drop event');
-            total +=1;
-            console.log("Total Events Fired = "+total);
-            total = 0;
-        });
-    });
-});
-
-Blue line code
-
-$(clientFrameWindow.document.body).find('*').on('dragover', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $(clientFrameWindow.document.body).find('.reserved-drop-marker').remove();
-    //pana aici 
-    $(event.target).append("<p class='reserved-drop-marker'></p>");
-    console.log('Drag Over');
-});
-*/
-//FULLY TRANSLATED - Removing jQuery
